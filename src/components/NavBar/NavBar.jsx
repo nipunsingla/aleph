ea8c307c70bd7fb31673caf220f7d8b6ed004986
Aleph1812 <aleph.tech111@gmail.com>
2020-03-30 18:24:08 +0530

    Adding the landing page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,47 +1,48 @@
 import React from 'react';
-import { Navbar ,Nav,Button} from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 import { useAuth0 } from "../../auth/react-auth0-spa";
 
 const NavBar = () => {
 
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-   return (
-   <div>
-        <Navbar bd='light' expand='lg'>
-            <Navbar.Brand href="#home">
-                <img
-                    src="/logo.svg"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    alt="aleph's"
-                />
-            </Navbar.Brand>
-            <Nav className='ml-auto mr-5'>
+    return (
+        <div>
+            <Navbar bd='light' expand='lg'>
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="aleph's"
+                    />
+                </Navbar.Brand>
+                <Nav className='ml-auto mr-5'>
 
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
 
-      <Nav.Link href="#home">Home</Nav.Link>
-      
-     
-            {!isAuthenticated && (
-                <Button className='mx-4'  onClick={() => loginWithRedirect({})}>Log in</Button>
-            )}
+                    <Nav.Link href="#home">Home</Nav.Link>
 
-            {isAuthenticated && <Button className='mx-4' onClick={() => logout()}>Log out</Button>}
-        
-     
-      <Button className='bg-secondary'>
-        Sign Up
+
+                    {!isAuthenticated && (
+                        <Button className='mx-4' onClick={() => loginWithRedirect({})}>Log in</Button>
+                    )}
+
+                    {isAuthenticated && <Button className='mx-4' onClick={() => logout()}>Log out</Button>}
+
+
+                    <Button className='bg-secondary'>
+                        Sign Up
       </Button>
-            </Nav>
-        </Navbar>
+                </Nav>
+            </Navbar>
 
-    </div>
+        </div>
 
 
-)}
+    )
+}
 export default NavBar;
