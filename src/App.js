a86133fe0db5b252828ff328a368f15d5d3799a5
nipunsingla <piyushsingla2416@gmail.com>
2020-05-05 23:02:58 +0530

    right portion with chatting content,keyboard and some buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/homepage/homepage'
 import { Classroom2 } from './pages/classroom/Classroom2'
 import { Group } from './pages/group/group'
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import {Chat} from './pages/chat/chat.component'
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
             <Route exact path='/' component={Homepage} />
             <Route exact path='/classroom' component={Classroom2} />
             <Route exact path='/group' component={Group} />
+            <Route exact path='/chat' component={Chat}/>
           </Switch>
         </Router>
       </header>
---
+++ src/pages/chat/chat.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {RightChatComponent} from '../../components/rightchatcomponent/RightChatComponent'
+export const Chat =()=>(
+    <div>
+        <RightChatComponent/>
+    </div>
+)
