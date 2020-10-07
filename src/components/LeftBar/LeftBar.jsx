7cdead63e033359731531b09b7e95e72f068efdd
nipunsingla <piyushsingla2416@gmail.com>
2020-10-07 23:16:26 +0530

    corrected link

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -9,7 +9,7 @@ import status_sign from '../../icons/status_sign.svg';
 import { GroupSelectModal } from '../group/GroupSelectModal';
 import SimpleBar from 'simplebar-react';
 import NotificationBar from '../notificationBar/NotificationBar'
-
+import {Link} from 'react-router-dom';
 
 class LeftBar extends React.Component {
     state = {
@@ -36,13 +36,13 @@ class LeftBar extends React.Component {
                 <Profile />
                 <ul className="list-group ml-0">
 
-                    <a href='/feed' className="list-group-item border-0"><img className=" border-2" src={home} height='40rem' width='40rem' alt="logo" /></a>
-                    <a href='/notifications' className="list-group-item border-0" onClick={this.toggleNotif}><img src={noti_bell} height='40rem' width='40rem' alt="logo" /></a>
+                    <Link to='/feed' className="list-group-item border-0"><img className=" border-2" src={home} height='40rem' width='40rem' alt="logo" /></Link>
+                    <Link to='/notifications' className="list-group-item border-0" onClick={this.toggleNotif}><img src={noti_bell} height='40rem' width='40rem' alt="logo" /></Link>
 
-                    <a href='/group' className="list-group-item border-0"><img src={group} height='40rem' width='40rem' alt="logo" /></a>
-                    <a href='/chat' className="list-group-item border-0"><img src={chat} height='40rem' width='40rem' alt="logo" /></a>
-                    <a href='/classroom' className="list-group-item border-0"><img src={classroom} height='40rem' width='40rem' alt="logo" /></a>
-                    <a href='/status' className="list-group-item border-0"><img src={status_sign} height='40rem' width='40rem' alt="logo" /></a>
+                    <Link to='/group' className="list-group-item border-0"><img src={group} height='40rem' width='40rem' alt="logo" /></Link>
+                    <Link to='/chat' className="list-group-item border-0"><img src={chat} height='40rem' width='40rem' alt="logo" /></Link>
+                    <Link to='/classroom' className="list-group-item border-0"><img src={classroom} height='40rem' width='40rem' alt="logo" /></Link>
+                    <Link to='/status' className="list-group-item border-0"><img src={status_sign} height='40rem' width='40rem' alt="logo" /></Link>
 
 
                 </ul>
